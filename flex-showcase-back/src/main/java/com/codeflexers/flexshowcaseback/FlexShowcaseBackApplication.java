ab78333c1ce4eb12fc8091670a3a9bb23960b6c8
Seopia <wjdwltjq7289@gmail.com>
2024-10-15 03:03:08 +0900

    first commit

---
+++ flex-showcase-back/src/main/java/com/codeflexers/flexshowcaseback/FlexShowcaseBackApplication.java
@@ -0,0 +1,13 @@
+package com.codeflexers.flexshowcaseback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexShowcaseBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexShowcaseBackApplication.class, args);
+    }
+
+}
---
+++ flex-showcase-back/src/test/java/com/codeflexers/flexshowcaseback/FlexShowcaseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeflexers.flexshowcaseback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexShowcaseBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
